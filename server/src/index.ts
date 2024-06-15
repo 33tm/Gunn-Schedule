71e310c073b13f47533246ee82c24bf24c2404b4
33tm <80350771+33tm@users.noreply.github.com>
2024-06-14 22:37:27 -0700

    resource checking + error handling

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,13 @@
+import { readdir } from "fs/promises"
 import { scrape } from "./scrape"
 
-scrape()
+await scrape()
+
+const files = await readdir("out")
+
+console.log(
+    files.map(file => {
+        const { updated } = require(`out/${file}`)
+        return { resource: file.slice(0, -5), updated }
+    })
+)
