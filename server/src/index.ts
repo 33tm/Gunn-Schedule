7b0041fc9472f4731cccb140c2bbe9f596d76cf5
33tm <80350771+33tm@users.noreply.github.com>
2024-06-20 19:09:10 -0700

    restructure + sqlite

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,12 @@
-import express, {
-    json,
-    type Request,
-    type Response
-} from "express"
-
-import {
-    existsSync,
-    mkdirSync,
-    readdirSync,
-    statSync,
-    writeFileSync
-} from "fs"
+import { existsSync, mkdirSync, readdirSync, statSync, writeFileSync } from "fs"
+import { createServer } from "http"
 import { createHash } from "crypto"
+import { Database } from "bun:sqlite"
+import { dirname } from "path"
+
+import express, { json, type Request, type Response } from "express"
+import { Server, type Socket, type Namespace } from "socket.io"
+import cors from "cors"
 
 import type { Menu } from "types"
 
@@ -27,35 +22,58 @@ await Promise.all(scrapers.map(scraper => scraper()))
     .then(res => writeFileSync("out/data.json", JSON.stringify(res)))
 
 // @ts-ignore
-export const data = await import("out/data.json")
-    .then(module => module.default)
-
+export const { default: data } = await import("out/data.json")
+export const db = new Database("db.sqlite", { create: true })
 export const menus = new Map<string, Menu>()
 
-const rest = express()
-    .use(json())
+const config = { origin: "*" }
+
+const rest = express().use(json(), cors(config))
+const server = createServer(rest)
+const socket = new Server(server, { cors: config })
 
 export type Route = (req: Request, res: Response) => void
+export type Handler = (server: Namespace, socket: Socket, data: any) => void
 
-const importRoutes = (root: string) => {
+const routes = (root: string) => {
     readdirSync(root).forEach(file => {
         const path = `${root}/${file}`
-        if (statSync(path).isDirectory()) return importRoutes(path)
+        if (statSync(path).isDirectory()) return routes(path)
         if (!file.endsWith(".ts")) return
         import(`./${path.slice(4, -3)}`).then(route => {
             const endpoint = path
-                .slice(8, -3)
+                .slice(15, -3)
                 .replace(/\[([^[\]]+)\]/g, ":$1")
                 .replace(/\/index$/, "") || "/"
             Object.entries(route).forEach(([method, handler]) => {
                 if (!(method.toLowerCase() in rest)) return
                 rest[method.toLowerCase() as keyof typeof rest](endpoint, handler)
-                console.log(`REST ${method} ${endpoint}`)
+                console.log(`${method} ${endpoint}`)
             })
         })
     })
 }
 
-importRoutes("src/rest")
+routes("src/routes/rest")
 
-rest.listen(443)
+const events = async (root: string) => {
+    readdirSync(root).forEach(file => {
+        const path = `${root}/${file}`
+        if (statSync(path).isDirectory()) return events(path)
+        if (!file.endsWith(".ts")) return
+        import(`./${path.slice(4, -3)}`).then(({ handler }) => {
+            if (!handler) return
+            const namespace = socket.of(`/${dirname(path).slice(11)}`)
+            namespace.on("connection", connection => {
+                connection.on(file.slice(0, -3), data => {
+                    handler(namespace, connection, data)
+                })
+            })
+            console.log(`Socket ${namespace.name} [${file.slice(0, -3)}]`)
+        })
+    })
+}
+
+events("src/routes/socket")
+
+server.listen(443)
