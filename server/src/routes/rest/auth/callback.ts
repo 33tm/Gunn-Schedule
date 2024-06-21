0fb3cdec190a6315c31ab300e3d0bc7c7f744dfa
33tm <80350771+33tm@users.noreply.github.com>
2024-06-21 11:52:16 -0700

    issue jwt

--- server/src/routes/rest/auth/callback.ts
+++ server/src/routes/rest/auth/callback.ts
@@ -4,7 +4,8 @@ import { sign } from "jsonwebtoken"
 export const POST: Route = async (req, res) => {
     const { id, key } = req.body
 
-    if (!tokens.has(id)) return res.status(401).end()
+    if (!tokens.has(id))
+        return res.status(401).end()
 
     const { secret } = tokens.get(id)!
 
@@ -13,50 +14,26 @@ export const POST: Route = async (req, res) => {
         .then(schoology.format)
         .catch(() => null)
 
-    if (!token) return res.status(401).end()
+    if (!token)
+        return res.status(401).end()
 
     const { api_uid: uid } = await schoology
         .request("GET", "/app-user-info", token)
 
-    if (!uid) return res.status(500).end()
+    if (!uid)
+        return res.status(500).end()
 
-    const user = db
-        .query("SELECT * FROM users WHERE uid = $uid")
-        .get({ $uid: uid })
+    const { building_id: bid, username } = await schoology
+        .request("GET", `/users/${uid}`, token)
 
-    if (!user) {
-        const { building_id: bid, name_display: name, username } = await schoology
-            .request("GET", `/users/${uid}`, token)
+    if (bid !== 7924989 || !username.match(/950\d{5}/))
+        return res.status(403).end()
 
-        if (bid !== 7924989) return res.status(403).end()
+    db
+        .query("INSERT OR IGNORE INTO users (uid, key, secret) VALUES ($uid, $key, $secret)")
+        .run({ $uid: uid, $key: key, $secret: secret })
 
-        const courses = await schoology
-            .request("GET", `/users/${uid}/sections`, token)
-            .then(({ section }: {
-                section: {
-                    course_title: string
-                    section_title: string
-                    profile_url: string
-                }[]
-            }) => section.map(({
-                course_title,
-                section_title,
-                profile_url
-            }) => ({
-                course: course_title,
-                section: section_title,
-                image: profile_url
-            })))
-
-        db
-            .query("INSERT INTO users (uid, id) VALUES ($uid, $id)")
-            .run({
-                $uid: uid,
-                $id: username
-            })
-    }
-
-    const jwt = sign({ uid }, process.env.JWT_SECRET!)
+    const jwt = sign({ uid, id: username }, process.env.JWT_SECRET!)
 
     socket.of("/auth").to(id).emit("jwt", jwt)
 
